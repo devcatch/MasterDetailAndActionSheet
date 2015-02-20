81e5eeb1d1036119bf9ca47f5aa038a61c14c37c
evbo <evbo@ciklum.com>
2015-02-20 15:47:39 +0200

    Added solution

---
+++ MasterDetailAndActionSheet.Shared/MasterDetailAndActionSheet.Shared/App.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MasterDetailAndActionSheet.Shared
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+            
+            var button = new Button { Text = "New root"};
+                        var contenPage = new ContentPage()
+            {
+                Content = new StackLayout()
+                {
+                    Padding = 20,
+                    Children = { button }
+                }
+            };
+
+            button.Clicked += (sender, e) =>
+            {
+                MainPage = new MenuMasterDetailPage();
+            };
+
+
+
+            MainPage = contenPage;
+
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ MasterDetailAndActionSheet.Shared/MasterDetailAndActionSheet.Shared/MenuMasterDetailPage.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MasterDetailAndActionSheet.Shared
+{
+    public class MenuMasterDetailPage : MasterDetailPage
+    {
+        public MenuMasterDetailPage ()
+        {
+            var mapPage = new ContentPage()
+            {
+                BackgroundColor = Color.Red
+            };
+
+            var logout = new Button { 
+                Text = "Log ud",
+                VerticalOptions = LayoutOptions.StartAndExpand
+            };
+
+            logout.Clicked += async (sender, e) => {
+                var action = await ShowDialogWithDiferentParamsForEachPlatform ();
+
+                if (action == "Logout")
+                {
+                    Master.Title = action;
+                }
+            };
+
+            Master = new ContentPage {
+                Title = "Menu",
+                Content = new StackLayout {
+                    Padding = 20,
+                    Children = {
+                        new Label { Text = "Hello Master" },
+                        logout
+                    }
+                }
+            };
+
+            Detail = new NavigationPage (mapPage);
+        }
+
+        async Task<string> ShowDialogWithDiferentParamsForEachPlatform ()
+        {
+            return await DisplayActionSheet(
+#if __IOS__
+                null, "Logout", "Cancel"
+#endif
+#if __ANDROID__
+"Are You shure?", null, null, "Cancel", "Logout"
+#endif
+#if WINDOWS_PHONE
+"Are You shure?", "Logout", "Cancel"
+#endif
+);
+        }
+    }
+}
+
+
